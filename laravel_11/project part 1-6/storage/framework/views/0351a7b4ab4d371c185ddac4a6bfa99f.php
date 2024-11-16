3aec0ddc6a7b5469b7de63046b17f7a0681f8b1b
NadiyahQasamah <145907307+NadiyahQasamah@users.noreply.github.com>
2024-11-17 00:41:46 +0700

    Delete laravel_11 directory

--- laravel_11/project part 1-6/storage/framework/views/0351a7b4ab4d371c185ddac4a6bfa99f.php
+++
@@ -1,6 +0,0 @@
-<a <?php echo e($attributes); ?>
-
-    class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">
-    <?php echo e($slot); ?>
-
-</a><?php /**PATH C:\Users\nadiy\Herd\project\resources\views/components/dropdown-link.blade.php ENDPATH**/ ?>
--- laravel_11/project part 1-6/storage/framework/views/4a96e8f67b9701586cc00e1cff6f701a.php
+++
@@ -1,4 +0,0 @@
-<a <?php echo e($attributes); ?> class="rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-<?php echo e($slot); ?>
-
-</a><?php /**PATH C:\Users\nadiy\Herd\project\resources\views/components/nav-link.blade.php ENDPATH**/ ?>
