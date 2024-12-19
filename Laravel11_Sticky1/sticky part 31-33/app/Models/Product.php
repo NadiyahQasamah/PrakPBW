c8eefbb90fdf9ce43b2c3bc75865900d225b0a46
NadiyahQasamah <nadiyahqass@gmail.com>
2024-12-19 22:19:37 +0700

    task

---
+++ Laravel11_Sticky1/sticky part 31-33/app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\Relationship;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'price',
+    ]; 
+
+    public function store(): BelongsTo
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+}
---
+++ Laravel11_Sticky1/sticky part 31-33/database/factories/StoreFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\StoreStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Store>
+ */
+class StoreFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => rand(1,10),
+            'name' => $title = str(fake()->word())->title(),
+            'slug' => str($title)->slug(),
+            'description' => fake()->paragraphs(2, true),
+            'status' => \App\Enums\StoreStatus::ACTIVE
+        ];
+    }
+}
